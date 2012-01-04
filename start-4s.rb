e192e0b5622c306e48450dd7c09561186b1b152b
Raphael Costa <raphael@raphaelcosta.net>
2012-01-04 13:15:17 -0200

    test

--- start-4s.rb
+++ start-4s.rb
@@ -6,6 +6,6 @@ require 'siriProxy'
 
 #Also try Eliza -- though it should really not be run "before" anything else.
 PLUGINS = [TestProxy]
-proxy = SiriProxy.new(PLUGINS,443,true)
+proxy = SiriProxy.new(PLUGINS, :port => 443, :proxy_4s => true)
 
 #that's it. :-)
