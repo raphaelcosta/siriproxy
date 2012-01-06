a4572a523fcc69d6dc1c1f64b93a4e8586e4e4eb
Raphael Costa <raphael@raphaelcosta.net>
2012-01-06 15:18:49 -0200

    test

--- plugins/testproxy/testproxy.rb
+++ plugins/testproxy/testproxy.rb
@@ -21,15 +21,6 @@ class TestProxy < SiriPlugin
 	####
 	# This gets called every time an object is received from an iPhone
 	def object_from_client(object, connection)
-		puts "TESTING"
-		puts connection
-		if connection.speechId == "f0a4b22c-5897-459a-ad89-6e9009f230b7"
-			puts "Raphael"
-			self.plugin_manager.block_rest_of_session_from_server
-			return generate_siri_utterance(connection.lastRefId, "Hello Raphael!")
-		end
-
-		
 		object
 	end
 	
@@ -53,6 +44,12 @@ class TestProxy < SiriPlugin
 	####
 	# This is called whenever the server recognizes speech. It's useful for overriding commands that Siri would otherwise recognize
 	def speech_recognized(object, connection, phrase)
+		if connection.speechId == "f0a4b22c-5897-459a-ad89-6e9009f230b7"
+			puts "Raphael"
+			self.plugin_manager.block_rest_of_session_from_server
+			return generate_siri_utterance(connection.lastRefId, "Hello Raphael!")
+		end
+		
 		if(phrase.match(/siri proxy map/i))
 			self.plugin_manager.block_rest_of_session_from_server
 			
