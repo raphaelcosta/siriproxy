cc30111f95de03202f8bc2afb3c30f7451a4464e
Raphael Costa <raphael@raphaelcosta.net>
2012-01-06 17:17:34 -0200

    test

--- web/app/admin/users.rb
+++ web/app/admin/users.rb
@@ -17,4 +17,14 @@ ActiveAdmin.register User do
     end
     f.buttons
   end
+
+  controller do
+
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
+    end
+  end
 end
