513cf044406ad5506f6df17c97c8d207996fd4df
Raphael Costa <raphael@raphaelcosta.net>
2012-01-06 16:36:02 -0200

    test

--- web/app/admin/users.rb
+++ web/app/admin/users.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :speech_id
+      f.input :assistant_id
+    end
+    f.buttons
+  end
 end
