d23428ff5e139465aa7bb9610a81b6b4cc019a6e
Raphael Costa <raphael@raphaelcosta.net>
2012-01-18 16:43:36 -0200

    fixes

--- web/app/admin/validations.rb
+++ web/app/admin/validations.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Validation do
   index do
     column :key do |v| v.key[0..40] end
     column :expired do |v| v.expired? ? 'Sim' : "Não" end
-    column :user do |v| v.device.user? ? v.device.user.name : "" end
+    column :user do |v| v.device.user ? v.device.user.name : "" end
     column :created_at
     default_actions
   end
